c22fc7873fd3a2db03053b5f3b6a2456e53f1374
XuNam45 <xunam0405@gmail.com>
2025-10-23 20:29:39 +0700

    Update ProductController: add getProductById and bind all methods

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -11,6 +11,7 @@ class ProductController {
   constructor() {
     this.createOrder = this.createOrder.bind(this);
     this.getOrderStatus = this.getOrderStatus.bind(this);
+    this.getProductById = this.getProductById.bind(this);
     this.ordersMap = new Map();
 
   }
@@ -85,7 +86,14 @@ class ProductController {
       res.status(500).json({ message: "Server error" });
     }
   }
-  
+ async getProductById(req, res, next) {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product) {
+      return res.status(404).json({ message: 'product not found' });
+    }
+    return res.status(200).json(product);
+  }
 
   async getOrderStatus(req, res, next) {
     const { orderId } = req.params;
@@ -95,7 +103,6 @@ class ProductController {
     }
     return res.status(200).json(order);
   }
-
   async getProducts(req, res, next) {
     try {
       const token = req.headers.authorization;
